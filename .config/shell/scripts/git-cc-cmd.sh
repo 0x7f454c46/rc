5306df086fe51e495379e8f8d171bb1361ea861b
Dmitry Safonov <0x7f454c46@gmail.com>
2016-12-30 18:42:37 +0300

    gitconfig: disable cccover and tweak cc-cmd
    Otherwise, cc-cmd script will put in CC for cover all from patches CCs
    and then cccover will put them on each patch from cover - which
    results in Ccing on each patch everyone from any patch.
    
    Signed-off-by: Dmitry Safonov <0x7f454c46@gmail.com>

--- .config/shell/scripts/git-cc-cmd.sh
+++ .config/shell/scripts/git-cc-cmd.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+function grep_cc()
+{
+	sed -n 's/^[cC][cC]:[[:blank:]]*\([[:graph:]].*\)/\1/p'
+}
+
 if [[ $(basename "${1}") =~ ^0000- ]] ; then
 	for p in $(dirname "${1}")/*.patch
 	do
-		cat "${p}" | sed -n					\
-			's/^[cC][cC]:[[:blank:]]*\([[:graph:]].*\)/\1/p'
+		cat "${p}" | grep_cc
 	done
+else
+	cat $(dirname "${1}")/0000-*.patch | grep_cc
 fi
 
