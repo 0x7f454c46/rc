0708369acdea19bb48c9c89001cad50bdb08a209
Dmitry Safonov <0x7f454c46@gmail.com>
2017-01-09 19:54:56 +0300

    git-cc-cmd: add 'reported-by' and other *-by in Cc
    Signed-off-by: Dmitry Safonov <0x7f454c46@gmail.com>

--- .config/shell/scripts/git-cc-cmd.sh
+++ .config/shell/scripts/git-cc-cmd.sh
@@ -4,13 +4,19 @@ function grep_cc()
 {
 	sed -n 's/^[cC][cC]:[[:blank:]]*\([[:graph:]].*\)/\1/p'
 }
+function grep_by()
+{
+	sed -n 's/^[[:graph:]]*-[bB][yY]:[[:blank:]]*\([[:graph:]].*\)/\1/p'
+}
 
 if [[ $(basename "${1}") =~ ^0000- ]] ; then
 	for p in $(dirname "${1}")/*.patch
 	do
 		cat "${p}" | grep_cc
+		cat "${p}" | grep_by
 	done
 else
 	cat $(dirname "${1}")/0000-*.patch | grep_cc
+	cat "${1}" | grep_by
 fi
 
